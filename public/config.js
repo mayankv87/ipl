33f35dea464895aaed51d6447e76956d673a98cd
unknown <mayankv87@gmail.com>
2017-04-01 17:33:12 +0530

    first commit

---
+++ public/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBgVcSnpAxHItRh_5IU9cWya7Ag-F-1fmw",
+    authDomain: "node-app-b414a.firebaseapp.com",
+    databaseURL: "https://node-app-b414a.firebaseio.com",
+    projectId: "node-app-b414a",
+    storageBucket: "node-app-b414a.appspot.com",
+    messagingSenderId: "346510178321"
+  };
+  firebase.initializeApp(config);
