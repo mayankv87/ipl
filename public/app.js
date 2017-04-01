43751def98adfb047f8efb5b46518aaa67c7752f
unknown <mayankv87@gmail.com>
2017-04-01 17:50:42 +0530

    first commit

--- public/app.js
+++ public/app.js
@@ -17,21 +17,21 @@
  */
 // FirebaseUI config.
 var uiConfig = {
-  'callbacks': {
-    // Called when the user has been successfully signed in.
-    'signInSuccess': function(user, credential, redirectUrl) {
-      handleSignedInUser(user);
-      // Do not redirect.
-      return false;
-    }
-  },
-  // Opens IDP Providers sign-in flow in a popup.
-  'signInFlow': 'popup',
-  'signInOptions': [
+    'callbacks': {
+        // Called when the user has been successfully signed in.
+        'signInSuccess': function (user, credential, redirectUrl) {
+            handleSignedInUser(user);
+            // Do not redirect.
+            return false;
+        }
+    },
+    // Opens IDP Providers sign-in flow in a popup.
+    'signInFlow': 'popup',
+    'signInOptions': [
     // TODO(developer): Remove the providers you don't need for your app.
-    {
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      scopes: ['https://www.googleapis.com/auth/plus.login']
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            scopes: ['https://www.googleapis.com/auth/plus.login']
     }
     /*{
       provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
@@ -50,8 +50,8 @@ var uiConfig = {
       requireDisplayName: true
     }*/
   ],
-  // Terms of service url.
-  'tosUrl': 'https://www.google.com'
+    // Terms of service url.
+    'tosUrl': 'https://www.google.com'
 };
 
 // Initialize the FirebaseUI Widget using Firebase.
@@ -62,16 +62,16 @@ var currentUid = null;
 /**
  * Redirects to the FirebaseUI widget.
  */
-var signInWithRedirect = function() {
-  window.location.assign('/widget');
+var signInWithRedirect = function () {
+    window.location.assign('/widget');
 };
 
 
 /**
  * Open a popup with the FirebaseUI widget.
  */
-var signInWithPopup = function() {
-  window.open('/widget', 'Sign In', 'width=985,height=735');
+var signInWithPopup = function () {
+    window.open('/widget', 'Sign In', 'width=985,height=735');
 };
 
 
@@ -79,104 +79,105 @@ var signInWithPopup = function() {
  * Displays the UI for a signed in user.
  * @param {!firebase.User} user
  */
-var handleSignedInUser = function(user) {
-  currentUid = user.uid;
-  document.getElementById('user-signed-in').style.display = 'block';
-  document.getElementById('user-signed-out').style.display = 'none';
-  document.getElementById('name').textContent = user.displayName;
-  document.getElementById('email').textContent = user.email;
-  if (user.photoURL){
-    document.getElementById('photo').src = user.photoURL;
-    document.getElementById('photo').style.display = 'block';
-  } else {
-    document.getElementById('photo').style.display = 'none';
-  }
+var handleSignedInUser = function (user) {
+    currentUid = user.uid;
+    document.getElementById('user-signed-in').style.display = 'block';
+    document.getElementById('user-signed-out').style.display = 'none';
+    document.getElementById('name').textContent = user.displayName;
+    document.getElementById('email').textContent = user.email;
+    if (user.photoURL) {
+        document.getElementById('photo').src = user.photoURL;
+        document.getElementById('photo').style.display = 'block';
+    } else {
+        document.getElementById('photo').style.display = 'none';
+    }
 };
 
 
 /**
  * Displays the UI for a signed out user.
  */
-var handleSignedOutUser = function() {
-  document.getElementById('user-signed-in').style.display = 'none';
-//    document.getElementById('main-container').style.display = 'none';
-  document.getElementById('user-signed-out').style.display = 'block';
-  ui.start('#firebaseui-container', uiConfig);
+var handleSignedOutUser = function () {
+    document.getElementById('user-signed-in').style.display = 'none';
+    //    document.getElementById('main-container').style.display = 'none';
+    document.getElementById('user-signed-out').style.display = 'block';
+    ui.start('#firebaseui-container', uiConfig);
 };
 
-var showIplSchedule = function(){
-      document.getElementById('main-container').style.display = 'block';
-       
+var showIplSchedule = function () {
+    document.getElementById('main-container').style.display = 'block';
+
     var matchHtml = '';
-for (var key in iplMatches) {
-   if (iplMatches.hasOwnProperty(key)) {
-      
-       
-       for(var i=0;i<iplMatches[key].length;i++){
-           console.log('date: '+ iplMatches[key][i].date+"<br> Match: "+iplMatches[key][i].match);
-           matchHtml += "<tr><td>" + iplMatches[key][i].date + "</td>"+
-               "<td><input type='radio' name='iplMatch'>" + iplMatches[key][i].teamA + " Vs <input type='radio' name='iplMatch'>"+iplMatches[key][i].teamB+"</td></tr>";
-           
-       }
-       
-   }
-}
-document.getElementById('ipl-matches').innerHTML = "<table>+"+matchHtml+"+</table>";    
-    
+    for (var key in iplMatches) {
+        if (iplMatches.hasOwnProperty(key)) {
+
+
+            for (var i = 0; i < iplMatches[key].length; i++) {
+                console.log('date: ' + iplMatches[key][i].date + "<br> Match: " + iplMatches[key][i].match);
+                matchHtml += "<tr><td>" + iplMatches[key][i].date + "</td>" +
+                    "<td><input type='radio' name='iplMatch'>" + iplMatches[key][i].teamA + " Vs <input type='radio' name='iplMatch'>" + iplMatches[key][i].teamB + "</td></tr>";
+
+            }
+
+        }
+    }
+    document.getElementById('ipl-matches').innerHTML = "<table>+" + matchHtml + "+</table>";
+
 }
 
 // Listen to change in auth state so it displays the correct UI for when
 // the user is signed in or not.
-firebase.auth().onAuthStateChanged(function(user) {
-  // The observer is also triggered when the user's token has expired and is
-  // automatically refreshed. In that case, the user hasn't changed so we should
-  // not update the UI.
-  if (user) {
-      //window.location.href = '/dashboard.html';
+firebase.auth().onAuthStateChanged(function (user) {
+    // The observer is also triggered when the user's token has expired and is
+    // automatically refreshed. In that case, the user hasn't changed so we should
+    // not update the UI.
+    if (user) {
+        //window.location.href = '/dashboard.html';
 
-      
-    showIplSchedule();  
-  }else{
+
+        showIplSchedule();
+    } else {
         document.getElementById('main-container').style.display = 'none';
-  }
+    }
 
-  document.getElementById('loading').style.display = 'none';
-  document.getElementById('loaded').style.display = 'block';
-  user ? handleSignedInUser(user) : handleSignedOutUser();
-    
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('loaded').style.display = 'block';
+    user ? handleSignedInUser(user) : handleSignedOutUser();
+
 });
 
 /**
  * Deletes the user's account.
  */
-var deleteAccount = function() {
-  firebase.auth().currentUser.delete().catch(function(error) {
-    if (error.code == 'auth/requires-recent-login') {
-      // The user's credential is too old. She needs to sign in again.
-      firebase.auth().signOut().then(function() {
-        // The timeout allows the message to be displayed after the UI has
-        // changed to the signed out state.
-        
-        setTimeout(function() {
-          alert('Please sign in again to delete your account.');
-        }, 1);
-      });
-    }
-  });
+var deleteAccount = function () {
+    firebase.auth().currentUser.delete().catch(function (error) {
+        if (error.code == 'auth/requires-recent-login') {
+            // The user's credential is too old. She needs to sign in again.
+            firebase.auth().signOut().then(function () {
+                // The timeout allows the message to be displayed after the UI has
+                // changed to the signed out state.
+
+                setTimeout(function () {
+                    alert('Please sign in again to delete your account.');
+                }, 1);
+            });
+        }
+    });
 };
 
 
 /**
  * Initializes the app.
  */
-var initApp = function() {
-  document.getElementById('sign-out').addEventListener('click', function() {
-    firebase.auth().signOut();
-  });
-  document.getElementById('delete-account').addEventListener(
-      'click', function() {
-        deleteAccount();
-      });
+var initApp = function () {
+    document.getElementById('sign-out').addEventListener('click', function () {
+        firebase.auth().signOut();
+    });
+    document.getElementById('delete-account').addEventListener(
+        'click',
+        function () {
+            deleteAccount();
+        });
 };
 
 window.addEventListener('load', initApp);
